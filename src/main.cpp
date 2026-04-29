4fec579c7c745809816017daa1283ee20f62065e
Huiqiang Huang <huanghuiqiang@mychery.com>
2026-04-29 17:27:08 +0800

    add setup: base code

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include "vulkan/vulkan_core.h"
 #include <cstddef>
 #include <cstdint>
 #include <cstdlib>
@@ -16,61 +15,6 @@
 
 
 
-class Ch1_Instance {
-public:
-    Ch1_Instance() = default;
-
-    void run() {
-        createInstance();
-    }
-
-private:
-    VkInstance instance_                = VK_NULL_HANDLE;
-    VkDebugUtilsMessengerEXT debugMsgr_ = VK_NULL_HANDLE;
-
-    void createInstance() {
-        VkApplicationInfo appInfo {};
-        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-        appInfo.pApplicationName = "LearnVulkan";
-        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.pEngineName = "No Engine";
-        appInfo.apiVersion = VK_API_VERSION_1_4;
-
-        uint32_t count;
-        const char** glfwExts = glfwGetRequiredInstanceExtensions(&count);
-        std::vector<const char *> extensions(glfwExts, glfwExts + count);
-        extensions.push_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
-        extensions.push_back(VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME);
-
-        VkInstanceCreateInfo createInfo {};
-        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-        createInfo.pApplicationInfo = &appInfo;
-        createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
-        createInfo.ppEnabledExtensionNames = extensions.data();
-        createInfo.enabledLayerCount = 0;
-
-        createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
-
-        auto result = vkCreateInstance(&createInfo, nullptr, &instance_);
-        if (result != VK_SUCCESS) {
-            throw std::runtime_error("Failed to create instance");
-        }
-        std::cout << "Create Instance Success\n";
-    }
-
-    void cleanup() {
-        if (debugMsgr_ != VK_NULL_HANDLE) {
-            
-        }
-
-        if (instance_ != VK_NULL_HANDLE) {
-            vkDestroyInstance(instance_, nullptr);
-        }
-    }
-
-
-};
-
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 const char* TITLE = "LearnVulkan";
@@ -83,16 +27,31 @@ public:
         cleanup();
     }
 private:
+    GLFWwindow *window = nullptr;
+
     void initVulkan() {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window = glfwCreateWindow(WIDTH, HEIGHT, TITLE, nullptr, nullptr);
+        if (window == nullptr) {
+            std::cerr << "Failed to create window";
+            glfwTerminate();
+            throw std::runtime_error("Failed to create window");
+        }
+
 
     }
 
     void mainLoop() {
-
+        while(!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
     }
 
     void cleanup() {
-
+        glfwDestroyWindow(window);
+        glfwTerminate();
     }
 
 };
@@ -107,29 +66,4 @@ int main() {
         return EXIT_FAILURE;
     }
     return EXIT_SUCCESS;
-
-
-    // Ch1_Instance instance;
-    // instance.run();
-
-    glfwInit();
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, TITLE, nullptr, nullptr);
-    if (window == nullptr) {
-        std::cerr << "Failed to create window";
-        glfwTerminate();
-        return -1;
-    }
-
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-    std::cout << extensionCount << " extensions supported\n";
-
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-    }
-
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    return 0;
 }
